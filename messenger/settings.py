bf17b4b72602de79b547bd4002962742c10e05ec
smart <rsirius06@gmail.com>
2023-07-25 17:32:08 +0300

    ready to using

--- messenger/settings.py
+++ messenger/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'C:/LisBank/lisbank/bank/static/'
+STATIC_ROOT = 'C:/LFMSH/messenger/lfmsh//static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
